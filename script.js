950f65d1d18db32d053e72f812f3d7a5e53ad834
igorsemenovdrums <igorsemenov@gmx.com>
2024-02-23 23:00:08 +0200

    Lesson 10 modified

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const myObject = {};
 
 for (let i = 0; i < myArray.length; i++) {
 
-myObject.name = myArray[0];
-myObject.area = myArray[1];
-myObject.population = myArray[2];
-myObject.mainStreet = myArray[3];
-myObject.mayorsName = myArray[4];
+myObject['name'] = myArray[0]; 
+myObject['area'] = myArray[1]; 
+myObject['population'] = myArray[2]; 
+myObject['mainStreet'] = myArray[3]; 
+myObject['mayorsName'] = myArray[4];
 
 }
 
@@ -22,3 +22,27 @@ return myObject;
 const obj = myFunction(myArray);
 
 console.log(obj);
+
+// ___________________________________________________________________________
+
+
+const city = ['name', 'area', 'population', 'main street', 'mayors name'];
+
+
+function myFunc(arr) {
+
+    const object = {};
+
+    city.forEach((element, index) => {
+
+        object[element] = prompt(`Enter ${element}:`);
+
+     });
+
+     return object;
+    
+    };
+
+    const cityObj = myFunc(city);
+
+    console.log(cityObj);
